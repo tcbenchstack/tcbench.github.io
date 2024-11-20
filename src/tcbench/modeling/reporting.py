22e215b5bdbd15775441ab2db5c166ac131b94c8
Alessandro Finamore <finale80@gmail.com>
2024-11-20 21:55:32 +0100

    refactored reporting functions related confusion matrix computation and added related tests

--- src/tcbench/modeling/reporting.py
+++ src/tcbench/modeling/reporting.py
@@ -4,12 +4,172 @@ import polars as pl
 import numpy as np
 
 from numpy.typing import NDArray
-from typing import Iterable
+from typing import Iterable, List
+
+def _confusion_matrix_add_totals(
+    conf_mtx: pl.DataFrame,
+) -> pl.DataFrame:
+    # add horizontal sum
+    conf_mtx = conf_mtx.with_columns(
+        _total_=(
+            conf_mtx
+            .drop("y_true")
+            .sum_horizontal()
+        )
+    )
+    # compute vertical sum
+    total_row = (
+        conf_mtx
+        .drop("y_true")
+        .sum()
+        .with_columns(y_true=pl.lit("_total_"))
+        .select(conf_mtx.columns)
+    )
+    # combine everything
+    return pl.concat((
+        conf_mtx,
+        total_row
+    ))
+
+
+def _confusion_matrix_reorder_rows_columns(
+    conf_mtx: pl.DataFrame,
+    *,
+    expected_labels: List[str] | None,
+    order: str = "lexicographic",
+    descending: bool = False,
+) -> pl.DataFrame:
+    if order not in ("lexicographic", "samples"):
+        raise ValueError(
+            f"""Unrecognized {order=} can be "lexicographic" or "samples" """
+        )
+
+    # remove all totals (if any)
+    conf_mtx = (
+        conf_mtx
+        .drop("_total_", strict=False)
+        .filter(pl.col("y_true") != "_total_")
+    )
+    # find true labels
+    true_labels = (
+        conf_mtx
+        .select("y_true")
+        .to_numpy()
+        .squeeze()
+        .tolist()
+    )
+    # find predicted labels
+    pred_labels = conf_mtx.drop("y_true").columns
+
+    if len(pred_labels) < len(true_labels):
+        raise RuntimeError(
+            "The number of predicted classes cannot be lower than "
+            "the number of true classes: \n"
+            f"{true_labels=}\n"
+            f"{pred_labels=}"
+        )
+
+    # inject empty prediction columns (if needed)
+    # for predicted classes expected but not found
+    if expected_labels is not None:
+        conf_mtx = (
+            conf_mtx.with_columns(
+                **{
+                    col: pl.lit(0).cast(pl.Int64())
+                    for col in expected_labels
+                    if col not in pred_labels
+                }
+            )
+            .select("y_true", *expected_labels)
+        )
+        pred_labels = expected_labels
+
+    if order == "lexicographic":
+        true_labels = sorted(true_labels, reverse=descending)
+    elif order == "samples":
+        true_labels = (
+            pl.DataFrame({
+                "y_true": conf_mtx["y_true"],
+                "samples": conf_mtx.drop("y_true").sum_horizontal()
+            })
+            .sort("samples", descending=descending)
+            .to_series()
+            .to_list()
+        )
+
+    # Note: prediction might regard only a subset of classes,
+    # so the confusion matrix might have less rows than columns.
+    # In this case, the columns are rearranged so place first
+    # the true labels based on the predefined order, then
+    # the remainder in lexicographic order
+    sorted_labels = true_labels
+    if len(true_labels) < len(pred_labels):
+        columns = true_labels[:]
+        for col in sorted(pred_labels, reverse=descending):
+            if col not in columns:
+                columns.append(col)
+        sorted_labels = columns
+
+    label_to_idx = dict(zip(
+        sorted_labels, range(len(sorted_labels))
+    ))
+
+    # reorder columns
+    conf_mtx = conf_mtx.select(
+        "y_true",
+        *sorted_labels,
+    )
+
+    # reorder rows
+    conf_mtx = (
+        # inject a dummy column to define rows order
+        conf_mtx.with_columns(
+            y_true_order=(
+                pl.col("y_true")
+                .map_elements(
+                    function=lambda text: label_to_idx.get(text, -1),
+                    return_dtype=pl.UInt32
+                )
+            )
+        )
+        # ...and impose row order
+        .sort("y_true_order", "y_true", descending=False)
+        .drop("y_true_order")
+        # # ...and add total by row
+        # .with_columns(
+        #     _total_=pl.sum_horizontal(expected_labels)
+        # )        
+        # .fill_null(0)
+    )
+    return _confusion_matrix_add_totals(conf_mtx)
+
+
+def _confusion_matrix_normalize_rows(
+    conf_mtx: pl.DataFrame,
+) -> pl.DataFrame:
+    if (
+        "_total_" not in conf_mtx.columns 
+        or len(conf_mtx.filter(pl.col("y_true") == "_total_")) == 0
+    ):
+        conf_mtx = _confusion_matrix_add_totals(conf_mtx)
+    # remove columns totals
+    conf_mtx = conf_mtx.filter(pl.col("y_true") != "_total_")
+    return (
+        conf_mtx.select(
+            "y_true", 
+            *[
+                pl.col(col).truediv(pl.col("_total_"))
+                for col in conf_mtx.drop("y_true", "_total_").columns
+            ]
+        )
+    )
+
 
 def confusion_matrix(
     y_true: NDArray, 
     y_pred: NDArray, 
-    expected_labels: Iterable[str] = None, 
+    *,
+    expected_labels: List[str] | None = None, 
     order: str | None = "lexicographic",
     descending: bool = False,
     normalize: bool = False,
@@ -27,101 +187,23 @@ def confusion_matrix(
             on="y_pred",
             values="len"
         )
-    )
-
-    pred_labels = conf_mtx.drop("y_true").columns
-    if expected_labels is None:
-        expected_labels = pred_labels
-    
-    if order == "lexicographic":
-        expected_labels = sorted(expected_labels, reverse=descending)
-    elif order == "samples":
-        true_labels_order = (
-            pl.DataFrame({
-                "y_true": conf_mtx["y_true"],
-                "samples": conf_mtx.drop("y_true").sum_horizontal()
-            })
-            .sort("samples", descending=descending)
-            .to_series()
-            .to_list()
-        )
-
-        # first labels in common between true/pred
-        common = set(pred_labels).intersection(set(true_labels_order))
-        l = [
-            col
-            for col in true_labels_order
-            if col in common
-        ]
-        # ...then the remainder
-        for col in true_labels_order:
-            if col in l:
-                continue
-            l.append(col)
-        expected_labels = l
-
-    expected_label_idx = dict(zip(
-        expected_labels, range(len(expected_labels))
-    ))
-
-    # inject empty prediction columns (if needed)
-    conf_mtx = conf_mtx.with_columns(
-        **{
-            col: None
-            for col in expected_labels
-            if col not in pred_labels
-        }
-    ).select("y_true", *expected_labels)
-
-    conf_mtx = (
-        # inject a dummy column to define rows order
-        conf_mtx.with_columns(
-            y_true_order=(
-                pl.col("y_true")
-                .map_elements(
-                    function=lambda text: expected_label_idx.get(text, -1),
-                    return_dtype=pl.UInt32
-                )
-            )
-        )
-        # ...and impose row order
-        .sort("y_true_order", "y_true", descending=False)
-        .drop("y_true_order")
-        # total by row
-        .with_columns(
-            _total_=pl.sum_horizontal(expected_labels)
-        )        
         .fill_null(0)
     )
 
-    # total by column
-    conf_mtx = pl.concat(
-        (
-            conf_mtx, 
-            pl.concat(
-                (
-                    pl.DataFrame({"y_true": ["_total_"]}),
-                    conf_mtx.drop("y_true").sum()
-                ), 
-                how="horizontal"
-            )
-        ),
-        how="vertical"
+    if expected_labels is None:
+        expected_labels = conf_mtx.drop("y_true").columns
+    if order is None:
+        order = "lexicographic"
+
+    conf_mtx = _confusion_matrix_reorder_rows_columns(
+        conf_mtx, 
+        expected_labels=expected_labels,
+        order=order,
+        descending=descending,
     )
 
     if normalize:
-        conf_mtx = (
-            # normalize by row
-            conf_mtx.select(
-                "y_true", 
-                *[
-                    pl.col(col).truediv(pl.col("_total_"))
-                    for col in conf_mtx.drop("y_true", "_total_").columns
-                ]
-            )
-            # remove columns totals
-            .filter(pl.col("y_true") != "_total_")
-        )
+        conf_mtx = _confusion_matrix_normalize_rows(conf_mtx)
 
     return conf_mtx
 
@@ -218,6 +300,7 @@ def classification_report_from_confusion_matrix(
 
     return pl.concat((class_rep, extra_metrics))
 
+
 def classification_report(
     y_true: NDArray,
     y_pred: NDArray,
@@ -238,3 +321,67 @@ def classification_report(
         order=order, 
         descending=descending
     )
+
+def average_confusion_matrix(
+    data: List[pl.DataFrame],
+    expected_labels: List[str] | None = None, 
+    order: str | None = "lexicographic",
+    descending: bool = False,
+    normalize: bool = False,
+) -> pl.DataFrame:
+
+    def _reorder_rows_columns(
+            df: pl.DataFrame
+    ) -> tuple[pl.DataFrame, list[str]]:
+        _df = (
+            df
+            .drop("_total_")
+            .filter(pl.col("y_true") != "_total_")
+            .sort(by="y_true", descending=False)
+        )
+        _rows = (
+            _df["y_true"]
+            .to_numpy()
+            .squeeze()
+            .tolist()
+        )
+        _df = _df.select(_rows)
+        return _df, _rows
+
+    def _verify_labels(expected_labels, found_labels):
+        if expected_labels != found_labels:
+            raise RuntimeError(
+                f"Different rows for {idx=}\n"
+                f"{expected_labels=}\n"
+                f"{found_labels=}" 
+            )
+
+    confmtx, found_labels = _reorder_rows_columns(data[0])
+    mtx = confmtx.to_numpy()
+
+    if expected_labels is None:
+        expected_labels = found_labels
+    else:
+        _verify_labels(expected_labels, found_labels)
+
+    for idx, confmtx in enumerate(data[1:], start=2):
+        confmtx, found_labels = _reorder_rows_columns(confmtx)
+        _verify_labels(expected_labels, found_labels)
+        mtx += confmtx.to_numpy()
+
+    confmtx_avg = pl.DataFrame({
+        col_name: mtx[:, idx].tolist() + [mtx[:, idx].sum()]
+        for idx, col_name in enumerate(expected_labels)
+    })
+    confmtx_avg = (
+        confmtx_avg
+        .with_columns(
+            y_true=pl.Series(expected_labels + ["_total_"]),
+           _total_=confmtx_avg.sum_horizontal(), 
+        )
+        .select(
+            ["y_true"] + expected_labels + ["_total_"]
+        )
+    )
+
+    return confmtx_avg
---
+++ tests/test_modeling_reporting.py
@@ -0,0 +1,318 @@
+from pyarrow import uint32
+import pytest
+
+import polars as pl
+import numpy as np
+
+import polars.testing
+from tcbench.modeling import reporting
+
+
+@pytest.mark.parametrize("test_input,expected,options", [
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b", "_total_"],
+            a=[10, 0, 1, 11],
+            c=[0, 20, 1, 21],
+            b=[1, 5, 15, 21],
+            _total_=[11, 25, 17, 53],
+        )),
+        pl.DataFrame(dict(
+            y_true=["a", "b", "c", "_total_"],
+            a=[10, 1, 0, 11],
+            b=[1, 15, 5, 21],
+            c=[0, 1, 20, 21],
+            _total_=[11, 17, 25, 53],
+        )),
+        dict(
+            order="lexicographic", 
+            descending=False,
+            expected_labels=None,
+        ),
+    ),
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b", "_total_"],
+            a=[10, 0, 1, 11],
+            c=[0, 20, 1, 21],
+            b=[1, 5, 15, 21],
+            _total_=[11, 25, 17, 53],
+        )),
+        pl.DataFrame(dict(
+            y_true=["c", "b", "a", "_total_"],
+            c=[20, 1, 0, 21],
+            b=[5, 15, 1, 21],
+            a=[0, 1, 10, 11],
+            _total_=[25, 17, 11, 53],
+        )),
+        dict(
+            order="lexicographic", 
+            descending=True,
+            expected_labels=None,
+        ),
+    ),
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b", "_total_"],
+            a=[10, 0, 1, 11],
+            c=[0, 20, 1, 21],
+            b=[1, 5, 0, 6],
+            _total_=[11, 25, 2, 38],
+        )),
+        pl.DataFrame(dict(
+            y_true=["b", "a", "c", "_total_"],
+            b=[0, 1, 5, 6],
+            a=[1, 10, 0, 11],
+            c=[1, 0, 20, 21],
+            _total_=[2, 11, 25, 38],
+        )),
+        dict(
+            order="samples", 
+            descending=False,
+            expected_labels=None,
+        ),
+    ),
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b", "_total_"],
+            a=[10, 0, 1, 11],
+            c=[0, 20, 1, 21],
+            b=[1, 5, 0, 6],
+            _total_=[11, 25, 2, 38],
+        )),
+        pl.DataFrame(dict(
+            y_true=["c", "a", "b", "_total_"],
+            c=[20, 0, 1, 21],
+            a=[0, 10, 1, 11],
+            b=[5, 1, 0, 6],
+            _total_=[25, 11, 2, 38],
+        )),
+        dict(
+            order="samples", 
+            descending=True,
+            expected_labels=None,
+        ),
+    ),
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b", "_total_"],
+            a=[10, 0, 1, 11],
+            c=[0, 20, 1, 21],
+            b=[1, 5, 15, 21],
+            _total_=[11, 25, 17, 53],
+        )),
+        pl.DataFrame(dict(
+            y_true=["a", "b", "c", "_total_"],
+            a=[10, 1, 0, 11],
+            b=[1, 15, 5, 21],
+            c=[0, 1, 20, 21],
+            d=[0, 0, 0, 0],
+            _total_=[11, 17, 25, 53],
+        )),
+        dict(
+            order="lexicographic", 
+            descending=False,
+            expected_labels=["a", "b", "c", "d"]
+        ),
+    ),
+])
+def test__confusion_matrix_reorder_rows_columns(test_input, expected, options):
+    res = reporting._confusion_matrix_reorder_rows_columns(
+        test_input, **options
+    )
+    polars.testing.assert_frame_equal(
+        expected, res, check_exact=True
+    )
+    
+
+@pytest.mark.parametrize("test_input,expected", [
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b"],
+            a=[10, 0, 1],
+            c=[0, 20, 1],
+            b=[1, 5, 15],
+        )),
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b", "_total_"],
+            a=[10, 0, 1, 11],
+            c=[0, 20, 1, 21],
+            b=[1, 5, 15, 21],
+            _total_=[11, 25, 17, 53],
+        )),
+ )])
+def test_confusion_matrix_add_totals(test_input, expected):
+    res = reporting._confusion_matrix_add_totals(test_input)
+
+    polars.testing.assert_frame_equal(
+        expected.select("_total_"), 
+        res.select("_total_")
+    )
+    polars.testing.assert_frame_equal(
+        expected.filter(pl.col("y_true") == "_total_"), 
+        res.filter(pl.col("y_true") == "_total_"),
+        check_exact=True,
+    )
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b"],
+            a=[10, 0, 1],
+            c=[0, 20, 1],
+            b=[1, 5, 15],
+        )),
+        pl.DataFrame(dict(
+            y_true=["a", "c", "b"],
+            a=[10/11, 0.0, 1/17],
+            c=[0.0, 20/25, 1/17],
+            b=[1/11, 5/25, 15/17],
+        )),
+ )])
+def test__confusion_matrix_normalize_rows(test_input, expected):
+    res = reporting._confusion_matrix_normalize_rows(test_input)
+    polars.testing.assert_frame_equal(
+        expected, res, check_exact=False
+    )
+
+
+@pytest.mark.parametrize("input_data,expected", [
+    (
+        dict(
+            y_true=np.array(list("aaa bbb ccc".replace(" ", ""))),
+            y_pred=np.array(list("abb bca ccc".replace(" ", ""))),
+        ),
+        pl.DataFrame(dict(
+            y_true=list("abc") + ["_total_"],
+            a=np.array([1, 1, 0, 2], dtype=np.uint32),
+            b=np.array([2, 1, 0, 3], dtype=np.uint32),
+            c=np.array([0, 1, 3, 4], dtype=np.uint32),
+            _total_=np.array([3, 3, 3, 9], dtype=np.uint32),
+        ))
+    ),
+    (
+        dict(
+            y_true=np.array(list("aaabbbccc")),
+            y_pred=np.array(list("abbbcaccc")),
+            order="lexicographic",
+            descending=True,
+        ),
+        pl.DataFrame(dict(
+            y_true=list("cba") + ["_total_"],
+            c=np.array([3, 1, 0, 4], dtype=np.uint32),
+            b=np.array([0, 1, 2, 3], dtype=np.uint32),
+            a=np.array([0, 1, 1, 2], dtype=np.uint32),
+            _total_=np.array([3, 3, 3, 9], dtype=np.uint32),
+        ))
+    ),
+    (
+        dict(
+            y_true=np.array(list("bbbb aaaaaa cc".replace(" ", ""))),
+            y_pred=np.array(list("bcbc abbbcc ac".replace(" ", ""))),
+            order="samples",
+            descending=True,
+        ),
+        pl.DataFrame(dict(
+            y_true=list("abc") + ["_total_"],
+            a=np.array([1, 0, 1, 2], dtype=np.uint32),
+            b=np.array([3, 2, 0, 5], dtype=np.uint32),
+            c=np.array([2, 2, 1, 5], dtype=np.uint32),
+            _total_=np.array([6, 4, 2, 12], dtype=np.uint32),
+        ))
+    ),
+    (
+        dict(
+            y_true=np.array(list("bbbb aaaaaa cc".replace(" ", ""))),
+            y_pred=np.array(list("bcbc abbbcc ac".replace(" ", ""))),
+            order="samples",
+            descending=False,
+        ),
+        pl.DataFrame(dict(
+            y_true=list("cba") + ["_total_"],
+            c=np.array([1, 2, 2, 5], dtype=np.uint32),
+            b=np.array([0, 2, 3, 5], dtype=np.uint32),
+            a=np.array([1, 0, 1, 2], dtype=np.uint32),
+            _total_=np.array([2, 4, 6, 12], dtype=np.uint32),
+        ))
+    ),
+    (
+        dict(
+            y_true=np.array(list("aaabbbccc")),
+            y_pred=np.array(list("abbbcaccc")),
+            expected_labels=["a", "b", "c", "d", "e"],
+        ),
+        pl.DataFrame(dict(
+            y_true=list("abc") + ["_total_"],
+            a=np.array([1, 1, 0, 2], dtype=np.uint32),
+            b=np.array([2, 1, 0, 3], dtype=np.uint32),
+            c=np.array([0, 1, 3, 4], dtype=np.uint32),
+            d=np.zeros(4, dtype=np.int64),
+            e=np.zeros(4, dtype=np.int64),
+            _total_=np.array([3, 3, 3, 9], dtype=np.int64),
+        ))
+    ),
+    (
+        dict(
+            y_true=np.array(list("aaabbbccc")),
+            y_pred=np.array(list("abbbcaccc")),
+            order="lexicographic",
+            descending=True,
+            expected_labels=["a", "b", "c", "d", "e"],
+        ),
+        pl.DataFrame(dict(
+            y_true=list("cba") + ["_total_"],
+            c=np.array([3, 1, 0, 4], dtype=np.uint32),
+            b=np.array([0, 1, 2, 3], dtype=np.uint32),
+            a=np.array([0, 1, 1, 2], dtype=np.uint32),
+            e=np.zeros(4, dtype=np.int64),
+            d=np.zeros(4, dtype=np.int64),
+            _total_=np.array([3, 3, 3, 9], dtype=np.int64),
+        ))
+    ),
+])
+def test_confusion_matrix(input_data, expected):
+    from sklearn import metrics
+    res = reporting.confusion_matrix(**input_data)
+    polars.testing.assert_frame_equal(res, expected)
+
+    # re-verify the confusion matrix based on sklearn
+
+    # in case extra columns are injected, we need
+    # to remove them from the counting because
+    # sklearn always return a squared matrix
+    # while the computation only inject columns
+    labels = res.drop("y_true", "_total_").columns
+    true_labels = np.unique(input_data["y_true"])
+    if (
+        "expected_labels" in input_data 
+        and len(true_labels) < len(labels)
+    ):
+        labels = [ 
+            lab
+            for lab in labels
+            if lab in true_labels
+        ]
+
+    mtx_expected = metrics.confusion_matrix(
+        y_true=input_data["y_true"],
+        y_pred=input_data["y_pred"],
+        labels=labels,
+    )
+    mtx_found = (
+        res
+        .filter(pl.col("y_true") != "_total_")
+        .drop("y_true", "_total_")
+        .to_numpy()
+        [:len(labels), :len(labels)]
+    )
+    assert (mtx_expected == mtx_found).all()
+
+
+
+
+
+
+
+
+
